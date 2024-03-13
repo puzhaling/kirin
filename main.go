5d06457855d6640ab79339a4b6f196f5fc6809ce
Hermann Puzhalin <puzhaling@gmail.com>
2024-03-13 23:00:58 +1000

    fix: enable to 1day api connection now

--- main.go
+++ main.go
@@ -10,76 +10,104 @@ import (
 	"os"
 )
 
+type Headline struct {
+	EffectiveDate 		string 			`json:"EffectiveDate"`
+	EffectiveEpochDate 	int64    		`json:"EffectiveEpochDate"`
+	Severity 			int32           `json:"Severity"`
+	Text 				string	 		`json:"Text"`
+	Category 			string			`json:"Category"`
+	EndDate 			string			`json:"EndDate"`
+	EndEpochDate 		int64          	`json:"EndEpochDate"`
+	MobileLink 			string          `json:"MobileLink"`
+	Link 				string	        `json:"Link"`
+}
+
+type Maximum struct {
+	Value 		float32        	`json:"Value"`
+	Unit 		string         	`json:"Unit"`
+	UnitType 	int32      		`json:"UnitType"`
+}
+
+type Minimum struct {
+	Value 		float32       	`json:"Value"`
+	Unit 		string         	`json:"Unit"`
+	UnitType 	int32      		`json:"UnitType"`
+}
+
+type Temperature struct {
+	Minimum Minimum `json:"Minimum"`
+	Maximum Maximum `json:"Maximum"`
+}
+
+type Day struct {
+	Icon 					int32    `json:"Icon"`
+	IconPhrase 				string   `json:"IconPhrase"`
+	HasPrecipitation 		bool   	 `json:"HasPrecipitation"`
+	PrecipitationType 		string	 `json:"PrecipitationType"`
+	PrecipitationIntensity  string	 `json:"PrecipitationIntensity"`
+}
+
+type Night struct {
+	Icon 			 int32         `json:"Icon"`        
+	IconPhrase 		 string		   `json:"IconPhrase"`
+	HasPrecipitation bool		   `json:"HasPrecipitation"`
+}
+
+type DailyForecast struct {
+	Date 			string              `json:"Date"`
+	EpochDate 		int64             	`json:"EpochDate"`
+	Temperature 	Temperature			`json:"Temperature"`
+	Day 			Day 				`json:"Day"`
+	Night 			Night				`json:"Night"`
+	Sources 		[]string			`json:"Sources"`
+	MobileLink 		string				`json:"MobileLink"`
+	Link 			string				`json:"Link"`
+}
+
 type Weather struct {
-	Headline struct {
-		EffectiveDate string 				`json: "Headline.EffectiveDate"`
-		EffectiveEpochDate int64    		`json: "Headline.EffectiveEpochDate"`
-		Severity int32              		`json: "Headline.Severity"`
-		Text string	 						`json: "Headline.Text"`
-		Category string						`json: "Headline.Category"`
-		EndDate string						`json: "Headline.EndDate"`
-		EndEpochDate int64          		`json: "Headline.EndEpochDate"`
-		MobileLink string           		`json: "Headline.MobileLink"`
-		Link string	                		`json: "Headline.Link"`
-	}                               		`json: "Headline"`
-
-	DailyForecast struct {
-		Date string                 		`json: "DailyForecast.Date"`
-		EpochDate int64             		`json: "DailyForecast.EpochDate"`
-		Temperature struct {
-
-			Minimum struct {        
-				Value float32       		`json: "DailyForecast.Temperature.Minimum.Value"`
-				Unit string         		`json: "DailyForecast.Temperature.Minimum.Unit"`
-				UnitType int32      		`json: "DailyForecast.Temperature.Minimum.UnitType"`
-			}                       		`json: "DailyForecast.Temperature.Minimum"`
-
-			Maximum struct {
-				Value float32        		`json: "DailyForecast.Temperature.Maximum.Value"`
-				Unit string         		`json: "DailyForecast.Temperature.Maximum.Unit"`
-				UnitType int32      		`json: "DailyForecast.Temperature.Maximum.UnitType"`
-			}                       		`json: "DailyForecast.Temperature.Maximum"`
-		}									`json: "DailyForecast.Temperature"`
-		
-		Day struct {
-			Icon int32              		`json: "DailyForecast.Day.Icon"`
-			IconPhrase string       		`json: "DailyForecast.Day.IconPhrase"`
-			HasPrecipitation bool   		`json: "DailyForecast.Day.HasPrecipitation"`
-			PrecipitationType string		`json: "DailyForecast.Day.PrecipitationType"`
-			PrecipitationIntensity string	`json: "DailyForecast.Day.PrecipitationIntensity"`
-		}									`json: "DailyForecast.Day"`	
-
-		Night struct {
-			Icon int32          			`json: "DailyForecast.Night.Icon"`        
-			IconPhrase string				`json: "DailyForecast.Night.IconPhrase"`
-			HasPrecipitation bool			`json: "DailyForecast.Night.HasPrecipitation"`
-		}									`json: "DailyForecast.Night"`
-
-		Sources []string					`json: "Sources"`
-		MobileLink string					`json: "MobileLink"`
-		Link string							`json: "Link"`
-	}
+	Headline 	   Headline 	   `json:"Headline"`
+	DailyForecasts []DailyForecast `json:"DailyForecasts"`
 }	
 
-type Search struct {
-	Version int32 				`json: "Version"`
-	Key string	  				`json: "Key"`
-	Type string				    `json: "Type"`
-	Rank int32					`json: "Rank"`
-	LocalizedName string		`json: "LocalizedName"`
-	Country struct {
-		LocalizedName string	`json: "Country.LocalizedName"`
-	}							`json: "Country"`
-	AdministrativeArea struct {
-		ID string				`json: "AdministrativeArea.ID"`
-		LocalizedName string	`json: "AdministrativeArea.LocalizedName"`
-	}						    `json: "AdministrativeArea"`
+
+
+
+
+type Country struct {
+	LocalizedName string	`json:"LocalizedName"`
 }
+
+type AdministrativeArea struct {
+	ID 			  string	`json:"ID"`
+	LocalizedName string	`json:"LocalizedName"`
+}
+
+type Search struct {
+	Version 				int32 				`json:"Version"`
+	Key 					string	  			`json:"Key"`
+	Type 					string				`json:"Type"`
+	Rank 					int32				`json:"Rank"`
+	LocalizedName 			string				`json:"LocalizedName"`
+	Country 				Country				`json:"Country"`
+	AdministrativeArea 		AdministrativeArea 	`json:"AdministrativeArea"`
+}
+
+
 
 func (s *Search) Print() {
 	fmt.Println("LocalizedName: ", s.LocalizedName)
 	fmt.Println("Key: ", s.Key)
+	fmt.Println("Country: ", s.Country.LocalizedName)
 	fmt.Println()
+}
+
+func (w Weather) Print() {
+	fmt.Println(w.Headline.EffectiveDate)
+	fmt.Println(w.Headline.EffectiveDate)
+	fmt.Println(w.Headline.EffectiveDate)
+	fmt.Println(w.Headline.Text)
+	fmt.Println(w.DailyForecasts[0].Temperature.Minimum.Value, "F")
+ 	fmt.Println(w.DailyForecasts[0].Temperature.Maximum.Value, "F")
 }
 
 // command syntax: kirin City{,} Country
@@ -95,9 +123,8 @@ func main() {
 	name := args[0]
 	country := args[1]
 
-	apikey := "p8C9PIGpwqgUzbwc3KV9iCr68PFINML3"
-	q := "Moscow"
-	URL := "http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey="+apikey+"&q="+q
+	apikey := "Qk2u387CuUWAKibhWIcqmDm3xDKbaw4t"
+	URL := "http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey="+apikey+"&q="+name
 
 	resp, err := client.Get(URL)
 	if err != nil {
@@ -115,24 +142,36 @@ func main() {
 	for idx, search := range searches {
 		if search.LocalizedName == name && search.Country.LocalizedName == country {
 			searchIdx = idx
-			break
+			//break
 		}
+		search.Print()
 	}
 
 
-	URL = "http://dataservice.accuweather.com/forecasts/v1/daily/1day/"+searches[searchIdx].Key
+	URL = "http://dataservice.accuweather.com/forecasts/v1/daily/1day/"+searches[searchIdx].Key+"?apikey="+apikey
 
-	var result Weather
+	var weather Weather
 
 	resp, err = client.Get(URL)
 	if err != nil {
 		log.Fatal("redirect function fail / HTTP response fail")
 	}
 
-	body, err = ioutil.ReadAll(resp.Body)
-	resp.Body.Close()
-	err = json.Unmarshal(body, &result)
+	if resp.StatusCode == http.StatusOK {
+		body, err = ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
 
-	fmt.Println(result.DailyForecast.Temperature.Minimum.Value, "F")
-	fmt.Println(result.DailyForecast.Temperature.Maximum.Value, "F")
+		if json.Valid([]byte(body)) {
+			fmt.Println("json is valid")
+
+			err = json.Unmarshal([]byte(body), &weather)
+			if err != nil {
+				log.Fatalf("json encoding fail: %v", err)
+			}
+		}
+	} else {
+		log.Fatal("recieved non-200 response status code:" , resp.StatusCode)
+	}
+
+	weather.Print()
 }
